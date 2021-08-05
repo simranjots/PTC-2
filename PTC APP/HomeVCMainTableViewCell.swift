947427a45c5a449a1d46e2f5aee05c53107fb6cc
Jaldeep <jaldeepsenjaliya@gmail.com>
2021-08-05 01:21:09 -0400

    Added TableView in Home Screen

---
+++ PTC APP/HomeVCMainTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  HomeVCMainTableViewCell.swift
+//  PTC APP
+//
+//  Created by Jaldeep Patel on 2021-08-05.
+//  Copyright © 2021 Gurlagan Bhullar. All rights reserved.
+//
+
+import UIKit
+
+class HomeVCMainTableViewCell: UITableViewCell {
+
+    @IBOutlet var containerView: UIView!
+    @IBOutlet var documentImageView: UIImageView!
+    @IBOutlet var acitivityNameLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        Utilities.addShadowAndBorderToView(containerView)
+        containerView.layer.borderWidth = 0
+        containerView.layer.cornerRadius = 10
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+       
+    }
+
+}
--- PTC APP/HomeViewController.swift
+++ PTC APP/HomeViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    @IBOutlet var sideMenuTableView: UITableView!
-    @IBOutlet var sideMenuView: UIView!
+    //MARK: - MainView Outlets
     
+    @IBOutlet var mainTableView: UITableView!
+    @IBOutlet var mainView: UIView!
+    
+    //MARK: - MainView Dummy Data
+    let activityNameArray = ["Interview", "Seminar", "Internal Meeting", "Client Meeting", "Webinar"]
+    let activityDate = ["Aug 5, 2021", "Aug 6, 2021", "Aug 7, 2021", "Aug 8, 2021", "Aug 9, 2021"]
+    
+    
+    //MARK: - Menubar Outlets
+    @IBOutlet var menubarTableView: UITableView!
+    @IBOutlet var menubarView: UIView!
+    
+    //MARK: - Menubar Items and Icons
     let menuItems = ["Home", "Profile", "Use Instructions", "About Author", "Privacy Policy", "Contact Us", "Sign Out"]
     let menuIcons = ["home", "profile", "instructions", "about-author", "privacy-policy", "contact-us", "sign-out"]
     var isSideViewOpened: Bool = false
@@ -21,47 +33,46 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 
         //Hide Menubar on ViewDidLoad
-        sideMenuView.isHidden = true
-        sideMenuTableView.backgroundColor = UIColor.systemBackground
+        menubarView.isHidden = true
+        menubarTableView.backgroundColor = UIColor.systemBackground
         isSideViewOpened = false
         
         //Add shadow to Menubar
-        Utilities.addShadowAndBorderToView(sideMenuView)
-        sideMenuView.layer.borderWidth = 0
+        Utilities.addShadowAndBorderToView(menubarView)
+        menubarView.layer.borderWidth = 0
         
     }
 
     
     @IBAction func sideMenuButtonTapped(_ sender: UIBarButtonItem) {
         
-        sideMenuView.isHidden = false
-        sideMenuTableView.isHidden = false
-        self.view.bringSubviewToFront(sideMenuView)
+        menubarView.isHidden = false
+        menubarTableView.isHidden = false
+        self.view.bringSubviewToFront(menubarView)
         
         if !isSideViewOpened {
             isSideViewOpened = true
-            sideMenuView.frame = CGRect(x: 0, y: 88, width: 0, height: 350)
-            sideMenuTableView.frame = CGRect(x: 0, y: 0, width: 0, height: 350)
+            menubarView.frame = CGRect(x: 0, y: 88, width: 0, height: 350)
+            menubarTableView.frame = CGRect(x: 0, y: 0, width: 0, height: 350)
             
             UIView.animate(withDuration: 0.7) {
-                self.sideMenuView.frame = CGRect(x: 0, y: 88, width: 250, height: 350)
-                self.sideMenuTableView.frame = CGRect(x: 0, y: 0, width: 250, height: 350)
+                self.menubarView.frame = CGRect(x: 0, y: 88, width: 250, height: 350)
+                self.menubarTableView.frame = CGRect(x: 0, y: 0, width: 250, height: 350)
             }
         } else {
-            sideMenuView.isHidden = true
-            sideMenuTableView.isHidden = true
+            menubarView.isHidden = true
+            menubarTableView.isHidden = true
             isSideViewOpened = false
 
-            sideMenuView.frame = CGRect(x: 0, y: 88, width: 250, height: 350)
-            sideMenuTableView.frame = CGRect(x: 0, y: 0, width: 250 , height: 350)
+            menubarView.frame = CGRect(x: 0, y: 88, width: 250, height: 350)
+            menubarTableView.frame = CGRect(x: 0, y: 0, width: 250 , height: 350)
 
             UIView.animate(withDuration: 0.7) {
-                self.sideMenuView.frame = CGRect(x: 0, y: 88, width: 0, height: 350)
-                self.sideMenuTableView.frame = CGRect(x: 0, y: 0, width: 250, height: 350)
+                self.menubarView.frame = CGRect(x: 0, y: 88, width: 0, height: 350)
+                self.menubarTableView.frame = CGRect(x: 0, y: 0, width: 250, height: 350)
             }
         }
     }
-    
     
     @IBAction func addActivityButtonTapped(_ sender: UIBarButtonItem) {
     
@@ -71,16 +82,44 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return menuItems.count
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        var numberOfRow = 0
+        
+        switch tableView {
+        case mainTableView:
+            numberOfRow = activityNameArray.count
+        case menubarTableView:
+            numberOfRow = menuItems.count
+        default:
+            print("Something's wrong!")
+        }
+        
+        return numberOfRow
+    }
+      
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CellIdentifiers.homeScreenMenuBarCell, for: indexPath) as! SideMenuTableViewCell
+        let cell = UITableViewCell()
         
-        cell.menuLabel.text = menuItems[indexPath.row]
-        cell.menuIconImageView.image = UIImage(named: menuIcons[indexPath.row])
+        switch tableView {
+        case mainTableView:
+            let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CellIdentifiers.homeScreenMainTableViewCell, for: indexPath) as! HomeVCMainTableViewCell
+            cell.acitivityNameLabel.text = activityNameArray[indexPath.row]
+            cell.dateLabel.text = activityDate[indexPath.row]
+            return cell
+        case menubarTableView:
+            let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CellIdentifiers.homeScreenMenuBarCell, for: indexPath) as! SideMenuTableViewCell
+            cell.menuLabel.text = menuItems[indexPath.row]
+            cell.menuIconImageView.image = UIImage(named: menuIcons[indexPath.row])
+            return cell
+        default:
+            print("Something's wrong!")
+        }
         
         return cell
     }
@@ -88,62 +127,71 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch tableView {
+        
+        case mainTableView:
+            print("MainTableView's cells are Tapped")
+        
+        case menubarTableView:
+            switch menuItems[indexPath.row] {
+        
+            case "Home":
+                let homeVC = self.storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.homeViewController) as! HomeViewController
+                self.navigationController?.pushViewController(homeVC, animated: true)
+                break
                 
-        switch menuItems[indexPath.row] {
-    
-        case "Home":
-            let homeVC = self.storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.homeViewController) as! HomeViewController
-            self.navigationController?.pushViewController(homeVC, animated: true)
-            break
-            
-        case "Profile":
-            performSegue(withIdentifier: Constants.Segues.homeToProfileSegue, sender: self)
-            break
-            
-        case "Use Instructions":
-            performSegue(withIdentifier: Constants.Segues.homeToUseInstructionsSegue, sender: self)
-            break
-            
-        case "About Author":
-            performSegue(withIdentifier: Constants.Segues.homeToAboutAuthor, sender: self)
-            break
-            
-        case "Privacy Policy":
-            performSegue(withIdentifier: Constants.Segues.homeToPrivacyPolicySegue, sender: self)
-            break
-            
-        case "Contact Us":
-            performSegue(withIdentifier: Constants.Segues.homeToContactUsSegue, sender: self)
-            break
-            
-        case "Sign Out":
-            print("Sign Out")
-//            if (userObject != nil) {
-//
-//                let resultFlag = currentUser.updateLoginStatus(status: false, email: (userObject?.email)!)
-//
-//                if (resultFlag == 0) {
-//                    let firebaseAuth = Auth.auth()
-//                    do {
-//                      try firebaseAuth.signOut()
-//                    } catch let signOutError as NSError {
-//                      print ("Error signing out: %@", signOutError)
-//                    }
-//                    let storyboard = UIStoryboard(name: "Login", bundle: nil)
-//                    let vc = storyboard.instantiateViewController(withIdentifier: "newLoginOptions") as! UINavigationController
-//                    self.present(vc, animated: true, completion: nil)
-//
-//                } else {
-//                    showAlert(title: "Error!", message: "Failed to update login status.", buttonTitle: "Try Again")
-//                }
-//            } else {
-//                showAlert(title: "Error!", message: "There's problem in loging out.", buttonTitle: "Try Again")
-//            }
-            break
-            
+            case "Profile":
+                performSegue(withIdentifier: Constants.Segues.homeToProfileSegue, sender: self)
+                break
+                
+            case "Use Instructions":
+                performSegue(withIdentifier: Constants.Segues.homeToUseInstructionsSegue, sender: self)
+                break
+                
+            case "About Author":
+                performSegue(withIdentifier: Constants.Segues.homeToAboutAuthor, sender: self)
+                break
+                
+            case "Privacy Policy":
+                performSegue(withIdentifier: Constants.Segues.homeToPrivacyPolicySegue, sender: self)
+                break
+                
+            case "Contact Us":
+                performSegue(withIdentifier: Constants.Segues.homeToContactUsSegue, sender: self)
+                break
+                
+            case "Sign Out":
+                print("Sign Out")
+    //            if (userObject != nil) {
+    //
+    //                let resultFlag = currentUser.updateLoginStatus(status: false, email: (userObject?.email)!)
+    //
+    //                if (resultFlag == 0) {
+    //                    let firebaseAuth = Auth.auth()
+    //                    do {
+    //                      try firebaseAuth.signOut()
+    //                    } catch let signOutError as NSError {
+    //                      print ("Error signing out: %@", signOutError)
+    //                    }
+    //                    let storyboard = UIStoryboard(name: "Login", bundle: nil)
+    //                    let vc = storyboard.instantiateViewController(withIdentifier: "newLoginOptions") as! UINavigationController
+    //                    self.present(vc, animated: true, completion: nil)
+    //
+    //                } else {
+    //                    showAlert(title: "Error!", message: "Failed to update login status.", buttonTitle: "Try Again")
+    //                }
+    //            } else {
+    //                showAlert(title: "Error!", message: "There's problem in loging out.", buttonTitle: "Try Again")
+    //            }
+                break
+                
+            default:
+                print("Screen is not available.")
+                break
+            }
         default:
-            print("Screen is not available.")
-            break
+            print("Something's wrong!")
         }
     }
 }
