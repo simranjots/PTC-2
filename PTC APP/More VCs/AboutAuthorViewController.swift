47c46a19f9e31e110a2ca148294fa87de965dff3
Jaldeep <jaldeepsenjaliya@gmail.com>
2021-08-06 02:45:43 -0400

    Updated About Author Screen

--- PTC APP/More VCs/AboutAuthorViewController.swift
+++ PTC APP/More VCs/AboutAuthorViewController.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class AboutAuthorViewController: UIViewController {
 
+    //MARK: - IBOutlets
+    @IBOutlet var authorImage: UIImageView!
+    @IBOutlet var authorNameLabel: UILabel!
+    @IBOutlet var aboutAuthorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        authorImage.image = UIImage(named: "teresa")
+        authorImage.layer.cornerRadius = 70
+        authorImage.layer.borderColor = Utilities.secondaryTextColor.cgColor
+        authorImage.layer.borderWidth = 2
+        authorNameLabel.text = "Teresa B. Easler"
+        //aboutAuthorLabel.text = ""
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+//For as far back as she can remember Teresa has always been passionate about the power of good communication. In fact, it was an incident that happened in her formative years that truly shaped the trajectory of her life. When Teresa was in grade school in the late 1950s, she was punished for talking out of turn in class (I know we were shocked too!). The teacher decided that her “punishment” was to be seated next to the only African American child in the class. Teresa certainly didn’t see this as much of a punishment for herself, but even at that young age she was acutely aware of how humiliating this must have been for the other child. That incident was the beginning of her commitment to seeing things from other peoples’ perspectives, and the power of both verbal, and non-verbal communication.  Whether guiding business leaders to reach new heights, coaching individuals to increase results by bettering their presentation skills or mentoring women of all ages, Teresa works with joyful determination to ensure her clients leave their time together challenged, and in an enhanced position to reach their goals. Teresa has found there are certain universal principles that work consistently. “When we communicate from the best of who we are,” she says, “from what’s most important to us, we inspire ourselves and we can inspire others. There’s communication, not just data exchange.”  When we look at the depth and diversity of Teresa’s entrepreneurial experiences, we often joke that it appears she has had many incarnations. She has done everything from running a successful gourmet cheesecake business from her home (yes, she was head baker too!), to running a company that offered high-end video production services to corporate clients. No matter what the business she quickly recognized that her success was rooted in good communication.  After many years of consulting with her corporate clients, Teresa realized companies needed help with their messaging, marketing and overall communication strategy, so she formed Connect To The Core Inc. because she truly believes at the core of all success is great communication, and at the core of all great communication is connection.  It is over 35 years since she started, and Teresa has helped thousands of individuals and hundreds of companies reach their goals. She is now in her element, creating original solutions to complex issues for her clients to become their best selves and achieve remarkable results.  Teresa was born and raised in Michigan and lived throughout the US for many years. She currently splits her time between her home in Toronto, Canada and her farm in Northern Michigan.
---
+++ PTC APP/More VCs/ContactUsViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ContactUsViewController.swift
+//  PTC APP
+//
+//  Created by Jaldeep Patel on 2021-08-05.
+//  Copyright © 2021 Gurlagan Bhullar. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ContactUsViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //MARK: - Outlet
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var knowMoreAboutUsButton: UIButton!
+    
+    let manager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupElements()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        manager.startUpdatingLocation()
+    }
+    
+
+    //MARK: - Map Rendering
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            render(location)
+        }
+    }
+    
+    func render(_ location: CLLocation) {
+        
+        //Coordinates of Connect to The Core
+        let latitude: CLLocationDegrees = 43.484677
+        let longitude: CLLocationDegrees = -79.643007
+        
+        //Set coordinate and span
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        
+        //Set region
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        //Add pin on map
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        mapView.addAnnotation(pin)
+        pin.title = "Connect To The Core"
+    }
+
+    //Set up and style the elements
+    func setupElements() {
+      
+        //Style purchase button
+        Utilities.styleButton(knowMoreAboutUsButton)
+        
+        //Style mapView
+        mapView.layer.borderColor = Utilities.secondaryTextColor.cgColor
+        mapView.layer.borderWidth = 1
+    }
+    
+    
+    //MARK: - IBActions
+    
+    @IBAction func knowMoreAboutUsButtonTapped(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://www.connecttothecore.com")!, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func instagramButtontapped(_ sender: UIButton) {
+       UIApplication.shared.open(URL(string: "https://www.instagram.com/connecttothecore/")!, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func facebookButtonTapped(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://www.facebook.com/Connect-To-The-Core-Inc-244517470006/")!, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func twitterButtonTapped(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://twitter.com/TeresaEasler")!, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func youtubeButtonTapped(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://www.youtube.com/channel/UCWjLdrHSw6nQDYXrCWQw9IA/playlists")!, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func linkedInButtonTapped(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2F802137%2Fadmin%2F")!, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func soundCloudButtonTapped(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://soundcloud.com/teresa-easler")!, options: [:], completionHandler: nil)
+    }
+}
